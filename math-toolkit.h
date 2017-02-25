e421d06ff5c9709f80a4c680cc446fabf189664c
xdennisx <denlee0125@gmail.com>
2017-02-26 00:06:05 +0800

    Use "loop unrolling" to optimize performance
    I try to unroll the for loop in function "dot_product" in math-toolkit.h to
    avoid branch. And the time decrease about 0.04 second.

--- math-toolkit.h
+++ math-toolkit.h
@@ -62,8 +62,11 @@ static inline
 double dot_product(const double *v1, const double *v2)
 {
     double dp = 0.0;
+    /*
     for (int i = 0; i < 3; i++)
         dp += v1[i] * v2[i];
+        */
+    dp = v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
     return dp;
 }
 
